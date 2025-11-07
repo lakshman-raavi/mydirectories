abf9519d7fb3e77d525d5aad9f65bc7696e54253
lakshman-raavi <2400030658@kluniversity.in>
2025-11-07 18:49:47 +0530

    myexecution

--- Labs-fedf/skill-7-exp/books.js
+++
@@ -1,6 +0,0 @@
-// ========== Book Data ==========
-export const books = [
-  { title: "Learn JavaScript", author: "A. Coder", price: 299.0, availability: "in stock" },
-  { title: "HTML & CSS", author: "B. Designer", price: 199.5, availability: "in stock" },
-  { title: "Advanced Algorithms", author: "C. Thinker", price: 399.99, availability: "out of stock" }
-];
